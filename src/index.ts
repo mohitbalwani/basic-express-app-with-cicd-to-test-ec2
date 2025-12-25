e0ffc8c81c0e649b29293d5e1fd707ca425fb102
mohitbalwani <mohitbalwani5@gmail.com>
2025-12-25 23:46:35 +0530

    Create new endpoint and change workflow branch

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,22 @@ app.get("/", (req: Request, res: Response) => {
   return res.send("It's working 🙌");
 });
 
-
-
+app.get("/quote", (req: Request, res: Response) => {
+  const quotes = [
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "Innovation distinguishes between a leader and a follower. - Steve Jobs",
+    "Life is what happens when you're busy making other plans. - John Lennon",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "It is during our darkest moments that we must focus to see the light. - Aristotle",
+    "The only impossible journey is the one you never begin. - Tony Robbins",
+    "Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill",
+    "Believe you can and you're halfway there. - Theodore Roosevelt"
+  ];
+  
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  
+  return res.json({ quote: randomQuote });
+});
 
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
 
